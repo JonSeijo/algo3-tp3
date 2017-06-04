fef4663a5b3d7c0224d3d9dfca06887c6933e69b
jonseijo <jon.seijo@gmail.com>
2017-06-04 15:09:04 -0300

    Agrego metodos para resolucion exacta

--- ResolverExacta.cpp
+++ ResolverExacta.cpp
@@ -2,8 +2,23 @@
 
 bool ResolverExacta::leerInput() {
     // Lee el input por stdin y guarda el grafo como lista de adyacencia
-    //     --> QUIZA convenga tenerlo como matriz de adyacencia, ver
-    return false;
+    //     --> QUIZA convenga tenerlo como *matriz* de adyacencia, ver
+
+    int n, m;
+    std::cin >> n >> m;
+
+    this->grafo.clear();
+    this->grafo.resize(n + 1, vector<int>(0));
+
+    for (int i = 0; i < m; i++) {
+        int v1, v2;
+        std::cin >> v1 >> v2;
+
+        this->grafo[v1].push_back(v2);
+        this->grafo[v2].push_back(v1);
+    }
+
+    return true;
 }
 
 void ResolverExacta::resolver(bool imprimirOutput) {
@@ -22,16 +37,40 @@ void ResolverExacta::resolver(bool imprimirOutput) {
     */
 }
 
+// Dice si los nodos forman un grafo completo
+// O(n^2)*O(sonVecinos)
 bool ResolverExacta::esClique(vector<int> &nodos) {
-    // Dice si los nodos forman un grafo completo
-    // Esperado: O(n + m)
-    return false;
+    // Quiero ver si todos los nodos son todos vecinos entre si
+    for (int v1 : nodos) {
+        for (int v2 : nodos) {
+            if (v1 != v2 && !sonVecinos(v1, v2)) {
+                return false;
+            }
+
+        }
+    }
+    return true;
 }
 
 // Pre: esClique(clique)
+// Da la cantidad de aristas que pertenecen a la frontera,
 int ResolverExacta::frontera(vector<int> &clique) {
-    // Da la cantidad de aristas que pertenecen a la frontera,
-    //  (asumiendo que clique es una clique)
-    // Esperado: O(n + m)
+
     return -1;
+}
+
+bool ResolverExacta::sonVecinos(int v1, int v2) {
+    // Optimizacion: recorrer los vecinos del que tiene menor cantidad
+    if (grafo[v2].size() < grafo[v1].size()) {
+        int tmp = v2;
+        v2 = v1;
+        v1 = tmp;
+    }
+
+    for (int w : grafo[v1]) {
+        if (w == v2) {
+            return true;
+        }
+    }
+    return false;
 }
--- ResolverExacta.h
+++ ResolverExacta.h
@@ -14,12 +14,14 @@ public:
 
 private:
     // Lista de adyacencia para representar al grafo
-    vector<vector<int> > vecinos;
+    vector<vector<int> > grafo;
 
     bool esClique(vector<int> &nodos);
 
     // Pre: esClique(clique)
     int frontera(vector<int> &clique);
+
+    bool sonVecinos(int v1, int v2);
 };
 
 #endif
